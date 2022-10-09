b16bdb2a183987948d94dddf2a38acea5029d8f7
Keshav Sharma <keshav2743@gmail.com>
2022-10-09 16:24:49 +0530

    profile photo updates

--- routes/comments.js
+++ routes/comments.js
@@ -3,10 +3,10 @@ const Comment = require('../controllers/comments');
 const express = require('express');
 const catchAsync = require('../utils/catchAsync');
 const router = express.Router({mergeParams: true});
-const {isLoggedIn, isCommentAuthor} = require('../middleware');
+const {isLoggedIn, isCommentAuthor, isAuthor} = require('../middleware');
 
 router.post('/',isLoggedIn, catchAsync(Comment.addComment));
 
-router.delete('/:commentId',isLoggedIn, isCommentAuthor,catchAsync(Comment.deleteComment));
+router.delete('/:commentId',isLoggedIn, isCommentAuthor,isAuthor,catchAsync(Comment.deleteComment));
 
 module.exports = router;
